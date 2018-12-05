657670ef2f7238aa5ecc90a42407e6003f21254c
iposokhin <iposokhin@gmail.com>
2018-12-06 01:12:47 +0500

    File api

--- Guano/src/js/script.js
+++ Guano/src/js/script.js
@@ -27,4 +27,31 @@ $(document).ready(function(){
     	},
   	]
   } );
+
+  let dropZone = document.querySelector('#hero-face'),
+  	  maxFileSize = 1000000;
+
+  dropZone.ondragover = ( e ) => {
+  	e.stopPropagation();
+  	e.preventDefault();
+  	e.dataTransfer.dropEffect = 'copy';
+  	console.dir( e );
+  	return false;
+  };
+
+
+  dropZone.ondrop = ( e ) => {
+  	e.stopPropagation();
+  	e.preventDefault();
+  	e.dataTransfer.dropEffect = 'copy';
+  	console.dir( e.dataTransfer.files[0] );
+  	let reader = new FileReader();
+  	reader.onload = ( event ) => {
+  		console.dir( event );
+  	}
+  	console.dir( reader.readAsDataURL(e.dataTransfer.files[0]) );
+
+  	return false;
+  } 
 });
+
