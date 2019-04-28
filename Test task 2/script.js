ffc4e096c53aabe53bbc7783b52eb8f25d4956cc
iposokhin <ivan.posokhin@yandex.ru>
2019-04-28 17:16:24 +0500

    test task 2

---
+++ Test task 2/script.js
@@ -0,0 +1,8 @@
+$( function() {
+  $( '.settings__brand' ).jScrollPane( {
+    'autoReinitialise': true,
+    'autoReinitialiseDelay': 10,
+    'verticalDragMinHeight': 0,
+    'verticalDragMaxHeight': 100
+  } );
+} );
